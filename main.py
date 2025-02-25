e9895b72b7a2088e9018fd25f167c59d7c38aa58
rogos30 <131408537+rogos30@users.noreply.github.com>
2025-02-25 13:51:25 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def kwadrat(liczba):
+    print(liczba*liczba)
+
+
+# Press the green button in the gutter to run the script.
+a = 5
+print(kwadrat(a))
+
