f2956871291e10773b1aab9e51c441626b2448e8
rogos30 <131408537+rogos30@users.noreply.github.com>
2025-02-25 14:34:22 +0100

    clustering

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
+from k_means import k_means
+import pandas as pd
 import numpy as np
 
-from decision_tree import DecisionTree
-from random_forest import RandomForest
-from load_data import generate_data, load_titanic
+
+def load_iris():
+    data = pd.read_csv("data/iris.data", names=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
+    print(data)
+    classes = data["class"].to_numpy()
+    features = data.drop("class", axis=1).to_numpy()
+    return features, classes
 
 
-def main():
-    np.random.seed(123)
+def evaluate(clusters, labels):
+    for cluster in np.unique(clusters):
+        labels_in_cluster = labels[clusters == cluster]
+        print(f"Cluster: {cluster}")
+        for label_type in np.unique(labels):
+            print(f"Num of {label_type}: {np.sum(labels_in_cluster == label_type)}")
+    
 
-    train_data, test_data = load_titanic()
-
-    dt = DecisionTree({"depth": 14})
-    dt.train(*train_data)
-    dt.evaluate(*train_data)
-    dt.evaluate(*test_data)
-
-    rf = RandomForest({"ntrees": 10, "feature_subset": 2, "depth": 14})
-    rf.train(*train_data)
-    rf.evaluate(*train_data)
-    rf.evaluate(*test_data)
+def clustering(kmeans_pp):
+    data = load_iris()
+    features, classes = data
+    intra_class_variance = []
+    for i in range(100):
+        assignments, centroids, error = k_means(features, 3, kmeans_pp)
+        evaluate(assignments, classes)
+        intra_class_variance.append(error)
+    print(f"Mean intra-class variance: {np.mean(intra_class_variance)}")
 
 
 if __name__ == "__main__":
-    main()
-
+    clustering(kmeans_pp = True)
+    # clustering(kmeans_pp = False)
